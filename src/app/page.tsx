2a2ce2e2768c1aebd58ba1bd0e65e4985a08a37f
Rakib-R <hasanrakib038@gmail.com>
2026-06-12 00:25:38 +0600

    CHanges testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="font-sans">NdR</main>;
+  return <main className="font-sans">Just for testing purposes</main>;
 }
